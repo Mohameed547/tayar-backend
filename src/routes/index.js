94addf0293d6bbf5ba3bcb39c6d743cb8e2a0d55
Gehad <gehadatef414@gmail.com>
2026-06-18 02:04:19 +0300

    feat: add support tickets module

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import shipmentRoutes from "../modules/shipments/shipments.routes.js";
+import supportRoutes from "../modules/support/support.routes.js";
 
 const router = Router();
 
 router.use("/shipments", shipmentRoutes);
+router.use("/support", supportRoutes);
 
 export default router;
