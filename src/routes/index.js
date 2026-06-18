dad1441acc9694e7f1859a8637c9427a11c6e4b3
Gehad <gehadatef414@gmail.com>
2026-06-18 19:16:02 +0300

    feat: add wallet module

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import shipmentRoutes from "../modules/shipments/shipments.routes.js";
 import supportRoutes from "../modules/support/support.routes.js";
+import walletRoutes from "../modules/wallet/wallet.routes.js";
 
 const router = Router();
 
 router.use("/shipments", shipmentRoutes);
 router.use("/support", supportRoutes);
-
+router.use("/wallet", walletRoutes);
 export default router;
